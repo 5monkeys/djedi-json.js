dfc3e884f0d6baecf965696989311ab28a1d2d15
Johan <johanskarholm@gmail.com>
2022-01-25 13:31:58 +0100

    workspaces (#7)
    * setup workspaces
    
    * Separate preview layer
    
    * Update Append button
    
    * Remove unused export
    
    * deprecate onChange,onSave in favour of using a consumer inside of the tree instead.

--- packages/djedi-json/src/contexts/cms.tsx
+++ packages/djedi-json/src/contexts/cms.tsx
@@ -6,16 +6,12 @@ import { TreeReducerAction } from '../core/Tree/types';
 export type CMSContextType = {
   config: Config;
   tree?: NodeTreeItem;
-  saveTree: () => void;
   setTree: (r: TreeReducerAction) => void;
 };
 
 const CMSContext = React.createContext<CMSContextType>({
   config: createConfig({}),
   tree: undefined,
-  saveTree: () => {
-    // not initiated
-  },
   setTree: () => {
     // not initiated
   },
--- packages/djedi-json/src/core/CMS/index.tsx
+++ packages/djedi-json/src/core/CMS/index.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 import CMSContext from '../../contexts/cms';
 import { createEmpty } from '../Node';
-import Preview from '../Renderer/Preview';
 import { reducer } from '../Tree';
 
 export interface CMSProps {
   config: Config;
   tree?: NodeTreeItem;
-  onChange?: (t: NodeTreeItem) => void;
-  onSave?: (t: NodeTreeItem) => void;
 }
 
 /**
@@ -20,18 +17,10 @@ export interface CMSProps {
 const CMS: React.FC<CMSProps> = ({
   tree: passedTree = createEmpty(''),
   config: passedConfig,
-  onChange,
-  onSave,
+  children,
 }) => {
   const [tree, setTree] = React.useReducer(reducer, passedTree);
   const [config, setConfig] = React.useState<Config>(passedConfig);
-
-  React.useEffect(() => {
-    onChange && onChange(tree);
-  }, [tree, onChange]);
-
-  // Save functionality to interface with components outside of the CMS.
-  const saveTree = () => onSave && onSave(tree);
 
   // keep tree in sync
   React.useEffect(() => setTree({ type: 'replace', payload: passedTree }), [passedTree]);
@@ -39,11 +28,7 @@ const CMS: React.FC<CMSProps> = ({
   // keep config in sync
   React.useEffect(() => setConfig(passedConfig), [passedConfig]);
 
-  return (
-    <CMSContext.Provider value={{ config, tree, setTree, saveTree }}>
-      <Preview />
-    </CMSContext.Provider>
-  );
+  return <CMSContext.Provider value={{ config, tree, setTree }}>{children}</CMSContext.Provider>;
 };
 
 export default CMS;
